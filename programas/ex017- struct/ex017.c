a3881264c6404854c139c69643d4f43a46196d80
Mateus <mat.allebrand@gmail.com>
2023-05-27 13:49:05 -0300

    update

--- programas/ex017- struct/ex017.c
+++ programas/ex017- struct/ex017.c
@@ -20,23 +20,23 @@ struct dados cliente[3];
 
 for (i=0; i<Tam; i++ ){
     printf("Nome: \n");
-    scanf("%s ", &cliente[i].nome);
+    scanf("%s", &cliente[i].nome);
     fflush(stdin);
 
-    printf("idade:\n ");
+    printf("idade: \n");
     scanf("%d", &cliente[i].idade);
     fflush(stdin);
         
-    printf("peso:");
-    scanf("%f ", &cliente[i].peso);
+    printf("peso: \n");
+    scanf("%f", &cliente[i].peso);
     fflush(stdin);
 }
 
 system("cls");
 
-for  (i=0; i<3; i++ ){
+for  (i=0; i<3; i++){
 printf(" dados da pessoa numero %d",i + 1);
-printf("Nome: %s \nIdade %d \nPeso: %.2f", cliente[i], cliente[i] , cliente[i]);
+printf("\nNome: %s \nIdade %d \nPeso: %.2f", cliente[i].nome, cliente[i].idade , cliente[i].peso);
 printf("\n");
 }
 
