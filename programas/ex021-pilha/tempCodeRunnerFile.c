0655fada72c6fe55b7c5b6e808076a0915ca8442
Mateus <mat.allebrand@gmail.com>
2023-06-20 17:56:16 -0300

    ex testes livro ptr

---
+++ programas/ex021-pilha/tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+p.dados[p.ultimo-1] = 0;  
+   p.ultimo--;
---
+++ programas/ex022-teste ponteiro/ex022.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+
+int main(){
+// int *ptr;
+// printf("Endereco: %p\n\n",ptr);
+// ptr = (int *) malloc(sizeof(int));
+// printf("Endereco: %p \nValor: %d",ptr,*ptr);
+
+// int x;
+// int *ptr;
+
+// x = 20;
+// ptr = &x;
+
+// printf("Endereco: %p \nValor: %d ",ptr,x);
+
+
+
+}
